fc6f77aabd3d199659036360a49b24f0ace565cc
Khaled <khaled.fkiri44@gmail.com>
2019-02-13 15:58:34 +0100

    Add home page and arjs

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var http = require('http');
-
+var fs = require('fs');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  fs.readFile('home.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
 }).listen(8080);
