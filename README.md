806c905d6536c83d06dc0cec79d8b768a84834e2
Spokeek <spokeek@gmail.com>
2019-02-13 16:42:41 +0100

    clean code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mspr_espi44
 ARjs la Réalité Augmentée
+Projet MSPR
--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('home.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
-}).listen(8080);
+var http = require('http')
+var fs = require('fs')
+
+const PORT = process.env.PORT || 8080
+
+const app = http.createServer((req, res) => {
+  try{
+    const homeFile = fs.readFileSync('home.html')
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(homeFile)
+    res.end()
+  }
+  catch(e){
+    res.writeHead(500, {'Content-Type': 'application/json'})
+    res.write(JSON.stringify(e))
+    res.end()
+  }
+})
+
+app.listen(PORT)
+
+console.log(`App started at port ${PORT} .`)
