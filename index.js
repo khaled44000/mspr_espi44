02e859ef5e9a94e5408c20bdbf512bc6fa2a3c1c
Spokeek <spokeek@gmail.com>
2019-03-11 21:36:27 +0100

    multi

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const PORT = process.env.PORT || 3000
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/page.html'))
+app.get('/multi', (req, res) => res.sendFile(__dirname + '/page-multi.html'))
 
 app.listen(PORT, () => console.log(`MSPR DEV app listening on port ${PORT}!`))
